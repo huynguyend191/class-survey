7e570cffab344225fc031e924847e50a32ca84a0
sneezingcat191 <huynguyend191@gmail.com>
2018-10-15 20:54:37 +0700

    Initial setup and create UploadFileArea component

---
+++ src/containers/UploadFileArea/index.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import Dropzone from 'react-dropzone';
+
+import styles from './UploadFileArea.module.css';
+import ExcelIcon from '../../assets/icons/excel.png';
+
+class UploadFileArea extends Component {
+  state = {
+    files: []
+  }
+
+  onSelectedFiles = (acceptedFiles, rejectedFiles) => {
+    if (rejectedFiles.length) {
+      alert('Not excel file');
+    }
+    let newFiles = this.state.files.concat(acceptedFiles);
+    this.setState({files: newFiles});
+  }
+
+  onUpload= () => {
+    console.log(this.state.files);
+  }
+
+  render() {
+    const openFileExplorer = React.createRef();
+    let selectedFiles = <p>Please drop the file here</p>;
+    let uploadAvailability = true;
+    if (this.state.files.length > 0) {
+      uploadAvailability =  false;
+      selectedFiles = (
+        this.state.files.map( file => {
+          return <li className={styles.Filename} key={file.lastModifiedDate}><img className={styles.ExcelIcon} src={ExcelIcon} alt="" />{file.name}</li>
+        })
+      )
+    }
+
+    return (
+      <div className={styles.UploadFileArea}>
+        <Dropzone 
+          className={styles.Dropzone}
+          onDrop={this.onSelectedFiles}
+          accept=".csv, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel"
+          disableClick
+          ref={openFileExplorer}
+        >
+          {selectedFiles}
+        </Dropzone>
+        <button onClick={() => openFileExplorer.current.open()}>Select file</button>
+        <button onClick={this.onUpload} disabled={uploadAvailability}>Upload</button>
+      </div>
+    );
+      
+      
+  }
+}
+
+export default UploadFileArea;
