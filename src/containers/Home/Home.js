23f36b0db6d2e85091e88b9d661ee70b37551f1d
sneezingcat191 <huynguyend191@gmail.com>
2018-11-16 23:06:17 +0700

    add content to Home

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,12 +1,31 @@
 import React, { Component } from 'react';
 import Calendar from 'react-calendar';
+import Clock from 'react-clock';
 import classes from './Home.module.css';
 
 class Home extends Component {
+
+  state = {
+    date: new Date(),
+  }
+
+  componentDidMount() {
+    setInterval(
+      () => this.setState({ date: new Date() }),
+      1000
+    );
+  }
+
   render() {
     return (
       <div className={classes.Home}>
-        <Calendar  value={new Date()}/>
+        <div className={classes.LeftArea}>
+          <h1>CLASS SURVEY SYSTEM</h1>
+        </div>
+        <div className={classes.RightArea}>
+          <Clock className={classes.Clock} value={this.state.date}/>
+          <Calendar className={classes.Calendar} value={new Date()}/>
+        </div>
       </div>
     );
   }
