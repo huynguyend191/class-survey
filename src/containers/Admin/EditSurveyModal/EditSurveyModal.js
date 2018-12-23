84a04f51b47f1194cf7c6f8c8efb477b743f8c23
sneezingcat191 <huynguyend191@gmail.com>
2018-12-23 11:05:02 +0700

    add version select, styling

--- src/containers/Admin/EditSurveyModal/EditSurveyModal.js
+++ src/containers/Admin/EditSurveyModal/EditSurveyModal.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import moment from 'moment';
 import { Dialog, IconButton, CircularProgress } from '@material-ui/core';
 import Close from '@material-ui/icons/Cancel';
 import classes from './EditSurveyModal.module.css';
@@ -14,29 +15,89 @@ class EditSurveyModal extends Component {
       this.props.onFetchSurveyVer();
     }
   }
+
   handleClose = () => (
     this.props.history.push(this.props.returnPath)
   )
+
   render() {
     let formBody = <div style={{display: 'flex', justifyContent: 'center', marginTop:'150px'}}><CircularProgress size={70}/></div>
     if (!this.props.loading) {
       if (this.props.surveyVersions.length > 0) {
-        formBody = (
-          <form>
-            <div>
-              <p></p>
-              <input />
+        const surveyVer =  this.props.surveyVersions;
+        const formatSurveyVer = [];
+        let createdDate = 'N/A';
+        let modifiedDate = 'N/A';
+        for (let index in surveyVer) {
+          if (surveyVer[index].CreatedDate) {
+            createdDate =  moment(surveyVer[index].CreatedDate).format('DD/MM/YYYY')
+          }
+          if (surveyVer[index].ModifiedDate) {
+            modifiedDate = moment(surveyVer[index].ModifiedDate).format('DD/MM/YYYY')
+          }
+          formatSurveyVer.push({
+            Id: surveyVer[index].Id,
+            Version: surveyVer[index].Version,
+            CreatedDate: createdDate,
+            ModifiedDate: modifiedDate
+          })
+          createdDate = 'N/A';
+          modifiedDate = 'N/A';
+        }
+        formBody = (  
+          <form className={classes.EditSurveyForm}>
+
+           <div className={classes.FormElement}>
+              <div className={classes.InputHolder}>
+                <p className={classes.InputTitle}>Open Date</p>
+                <DatePicker className={classes.Input} />
+              </div>
+              <div className={classes.ErrMsg}>{null}</div>
             </div>
-            <div>
-              <p></p>
-              <input />
+
+            <div className={classes.FormElement}>
+              <div className={classes.InputHolder}>
+                <p className={classes.InputTitle}>Close Date</p>
+                <DatePicker className={classes.Input} />
+              </div>
+              <div className={classes.ErrMsg}>{null}</div>
             </div>
-            <div>
-              <DatePicker />
+
+            <div className={classes.FormElement}>
+              <div className={classes.InputHolder}>
+                <p className={classes.InputTitle}>Survey Version</p>
+                <select className={classes.Select}>
+                  {
+                    formatSurveyVer.map(surveyVer => {
+                      return (
+                        <option key={surveyVer.Id} value={surveyVer.Id}>
+                          Ver{surveyVer.Version} - C:{surveyVer.CreatedDate} - M:{surveyVer.ModifiedDate} 
+                        </option>
+                      )
+                    })
+                  }
+                </select>
+              </div>
+              <div className={classes.ErrMsg}>Haha</div>
             </div>
-            <div>
-              <DatePicker />
+
+            <div className={classes.FormElement}>
+              <div className={classes.InputHolder}>
+                <p className={classes.InputTitle}>Class Code</p>
+                <input className={classes.Input} />
+              </div>
+              <div className={classes.ErrMsg}>Haha</div>
             </div>
+
+            <div className={classes.FormElement}>
+              <div className={classes.InputHolder}>
+                <p className={classes.InputTitle}>Title</p>
+                <input className={classes.Input} />
+              </div>
+              <div className={classes.ErrMsg}>{null}</div>
+            </div>
+
+           
           </form>
         )
       } else {
