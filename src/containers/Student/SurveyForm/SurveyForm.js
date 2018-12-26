38332d8642326927cd7a6c5ab54fd5ee15096c53
sneezingcat191 <huynguyend191@gmail.com>
2018-12-26 16:19:31 +0700

    add radio label for each category

--- src/containers/Student/SurveyForm/SurveyForm.js
+++ src/containers/Student/SurveyForm/SurveyForm.js
@@ -35,32 +35,52 @@ class SurveyForm extends Component {
   render() {
     let surveyForm = null;
     if (this.state.surveyContent) {
+      
       const formInfo = invertObjectServerToClient(this.props.surveyInfo.content);
-      surveyForm = Object.keys(formInfo).map((key, index) => {
+      let surveyFormContent = Object.keys(formInfo).map((key, index) => {
         const listItems = formInfo[key].map(item => {
           //return sub category
           return (
             <div key={item} className={classes.RadioGroup}>
-              <p>{item}</p>
+              <p style={{width: '60%'}}>{item}</p>
               <RadioGroup 
                 style={{ display: 'flex', flexDirection:'row' }} 
                 onChange={(event) => this.handleChange(event, item)}
                 value={this.state.surveyContent[item]}
-              >
-                <FormControlLabel value="1" control={<Radio color="primary" />} label="1" />
+              > 
+                <Radio color="primary" value="1" />
+                <Radio color="primary" value="2" />
+                <Radio color="primary" value="3" />
+                <Radio color="primary" value="4" />
+                <Radio color="primary" value="5" />
+
+                {/* <FormControlLabel value="1" control={<Radio color="primary" />} label="1" />
                 <FormControlLabel value="2" control={<Radio color="primary" />} label="2" />
                 <FormControlLabel value="3" control={<Radio color="primary" />} label="3" />
                 <FormControlLabel value="4" control={<Radio color="primary" />} label="4" />
-                <FormControlLabel value="5" control={<Radio color="primary" />} label="5" />
+                <FormControlLabel value="5" control={<Radio color="primary" />} label="5" /> */}
               </RadioGroup>
             </div>
             
           )
         })
         return (
-          <div key={key}><p>{index+1}. {key}</p>{listItems}</div>
+          <div key={key} style={{borderBottom: '1px solid rgb(163, 163, 163)'}}>
+            <div style={{display: 'flex', flexDirection:'row', justifyContent: 'space-between'}}>
+              <p style={{fontWeight: '500'}}>{index+1}. {key}</p>
+              <div style={{display: 'flex', flexDirection:'row'}}>
+                <p className={classes.RadioLabel}>1</p>
+                <p className={classes.RadioLabel}>2</p>
+                <p className={classes.RadioLabel}>3</p>
+                <p className={classes.RadioLabel}>4</p>
+                <p className={classes.RadioLabel}>5</p>
+              </div>
+            </div>
+            {listItems}
+          </div>
         )
       })
+      surveyForm = <form>{surveyFormContent}</form>
     }
     
     return (
